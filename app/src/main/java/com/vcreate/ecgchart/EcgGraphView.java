51aca3fd51115377fcfa75285f1721997394733e
Vahid-Soudagar <88474649+Vahid-Soudagar@users.noreply.github.com>
2024-04-20 17:35:45 +0530

    Completed Background of the graph

--- app/src/main/java/com/vcreate/ecgchart/EcgGraphView.java
+++ app/src/main/java/com/vcreate/ecgchart/EcgGraphView.java
@@ -1,27 +1,40 @@
+/**
+ * EcgGraphView is a custom view for displaying an ECG graph with square grid lines.
+ * Author: Vahid Soudagar
+ */
+
 package com.vcreate.ecgchart;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
+import java.util.Random;
+
 public class EcgGraphView extends SurfaceView implements SurfaceHolder.Callback {
 
     private Paint gridPaint;
+    private int mHeight;
+    private int mWidth;
+
+    private boolean isSurfaceViewAvailable;
+    private Paint wavePaint;
 
     public EcgGraphView(Context context) {
-        super(context);;
+        this(context, null);
         init();
     }
 
     public EcgGraphView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
         init();
     }
 
@@ -31,22 +44,71 @@ public class EcgGraphView extends SurfaceView implements SurfaceHolder.Callback 
     }
 
 
-    private void init() {
-        getHolder().addCallback(this);
-        setBackgroundColor(ContextCompat.getColor(getContext(), R.color.ecg));
-        gridPaint = new Paint();
-        gridPaint.setColor(ContextCompat.getColor(getContext(), R.color.r1));
-        gridPaint.setStrokeWidth(2);
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        isSurfaceViewAvailable = true;
+
     }
 
     @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
 
+        // Define graph dimensions
+        float graphWidthMM = 250; // in mm
+        float graphHeightMM = 40; // in mm
+
+        // Convert mm to pixels
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float dpi = metrics.xdpi;
+        float pixelsPerMM = dpi / 25.4f; // 1 inch = 25.4 mm
+        int graphWidth = Math.round(graphWidthMM * pixelsPerMM);
+        int graphHeight = Math.round(graphHeightMM * pixelsPerMM);
+
+        // Ensure landscape orientation
+        int canvasWidth = getWidth();
+        int canvasHeight = getHeight();
+        if (canvasWidth < canvasHeight) {
+            // Swap canvasWidth and canvasHeight for landscape orientation
+            int temp = canvasWidth;
+            canvasWidth = canvasHeight;
+            canvasHeight = temp;
+        }
+
+        // Calculate the number of horizontal and vertical lines
+        int horizontalLines = (int) (graphHeightMM / 5); // Each line represents 5mm
+        int verticalLines = (int) (graphWidthMM / 5); // Each line represents 5mm
+
+        // Calculate spacing between grid lines
+        float deltaX = (float) graphWidth / verticalLines;
+        float deltaY = (float) graphHeight / horizontalLines;
+
+        // Ensure equal spacing for square grid cells
+        float cellSize = Math.min(deltaX, deltaY);
+        deltaX = cellSize;
+        deltaY = cellSize;
+
+        Log.d("TestData", "Length of each square (mm): " + cellSize);
+
+        // Draw horizontal grid lines
+        for (int i = 0; i <= horizontalLines; i++) {
+            float y = i * deltaY;
+            canvas.drawLine(0, y, canvasWidth, y, gridPaint);
+        }
+
+        // Draw vertical grid lines
+        for (int i = 0; i <= verticalLines; i++) {
+            float x = i * deltaX;
+            canvas.drawLine(x, 0, x, canvasHeight, gridPaint);
+        }
     }
+
+
+
+
 
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
     }
 
     @Override
@@ -55,23 +117,23 @@ public class EcgGraphView extends SurfaceView implements SurfaceHolder.Callback 
     }
 
     @Override
-    protected void onDraw(@NonNull Canvas canvas) {
-        super.onDraw(canvas);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        // Draw horizontal grid lines
-        int numHorizontalLines = 5; // Number of horizontal lines
-        float deltaY = (float) getHeight() / (numHorizontalLines + 1); // Gap between each horizontal line
-        for (int i = 1; i <= numHorizontalLines; i++) {
-            float y = i * deltaY;
-            canvas.drawLine(0, y, getWidth(), y, gridPaint);
-        }
-
-        // Draw vertical grid lines
-        int numVerticalLines = 10; // Number of vertical lines
-        float deltaX = (float) getWidth() / (numVerticalLines + 1); // Gap between each vertical line
-        for (int i = 1; i <= numVerticalLines; i++) {
-            float x = i * deltaX;
-            canvas.drawLine(x, 0, x, getHeight(), gridPaint);
-        }
+        int width = (MeasureSpec.getSize(widthMeasureSpec));
+        if (width > mWidth) mWidth = width;
+        int height = (int) (MeasureSpec.getSize(heightMeasureSpec) * 0.95);
+        if (height > mHeight) mHeight = height;
     }
+
+    private void init() {
+        setBackgroundColor(ContextCompat.getColor(getContext(), R.color.ecg));
+        wavePaint = new Paint();
+        gridPaint.setColor(ContextCompat.getColor(getContext(), R.color.black));
+        gridPaint.setStrokeWidth(2);
+        gridPaint = new Paint();
+        gridPaint.setColor(ContextCompat.getColor(getContext(), R.color.r1));
+        gridPaint.setStrokeWidth(2);
+    }
+
 }
